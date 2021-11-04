cc2bb0f433835be293dcd233c4b4054195157a3f
DanyalAslam <send2danyalaslam@gmail.com>
2021-11-03 23:25:57 -0700

    lice url

--- src/Api/Config.js
+++ src/Api/Config.js
@@ -5,9 +5,13 @@ const config = {
     // url: 'https://dev20.onlinetestingserver.com/h-empirical/api/',
     // url: 'http://dev74.onlinetestingserver.com/estreams/api'
 
-    // url:'http://103.25.138.171:4080/UBI-APP/',
-    url :'http://unibro.com.pk/UBI-APP/',
-    ReportUrl :'http://unibro.com.pk/'
+    url:'http://103.25.138.171:4080/UBI-APP/',
+    // url :'http://unibro.com.pk/UBI-APP/',
+
+
+  
+    // ReportUrl :'http://unibro.com.pk/'
+    ReportUrl :'http://103.25.138.171:4080/'
 }
 
 export default config
